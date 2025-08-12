3bd90a54d4bc37f95aa4cf35f939fb923c71e20d
ValeriaSimon <155743104+ValeriaSimon@users.noreply.github.com>
2025-08-12 19:07:19 +0300

    Delete middleware.js

--- middleware.js
+++
@@ -1,4 +0,0 @@
-// middleware.js (temporary)
-import { NextResponse } from 'next/server';
-export const config = { matcher: ['/((?!_next/|.*\\.).*)'] };
-export function middleware() { return NextResponse.next(); }
